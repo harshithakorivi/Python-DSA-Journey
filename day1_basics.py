bfc2f50f39655bb2e805be5beaec64ebe38fd19d
harshithakorivi <harshithakorivi0421@gmail.com>
2025-12-31 10:37:29 +0530

    Add basic variable declarations and type prints

--- day1_basics.py
+++ day1_basics.py
@@ -1,1 +1,18 @@
 print("hello world")
+
+name="harshitha"
+age=19
+print(name)
+print(age)
+
+name=input("enter your name")
+print("hello",name)
+
+x=10
+y=4.5
+z="shivani"
+p=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(p))
